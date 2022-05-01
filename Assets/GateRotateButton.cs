fed55065c45732f9d951c972264a00230658e8db
Shestakov Nikolay <workjustghostw1ng@gmail.com>
2022-05-01 21:32:40 +0300

    Add buttonClick Sounds

--- Assets/GateRotateButton.cs
+++ Assets/GateRotateButton.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GateRotateButton : MonoBehaviour
 {
     [SerializeField] private PlatformRotate _gate;
+    [SerializeField] private AudioClip _buttonClip;
 
-    private void OnTriggerStay(Collider other)
+    private UnityAction ButtonClicked;
+
+    private void OnEnable()
+    {
+        ButtonClicked += OnButtonClicked;
+    }
+
+    private void OnDisable()
+    {
+        ButtonClicked -= OnButtonClicked;
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Player _player))
         {
             _gate.RotateGate();
+            ButtonClicked?.Invoke();
         } 
     }
 
     private void OnTriggerExit(Collider other)
     {
         _gate.BackGate();
+        ButtonClicked?.Invoke();
+    }
+
+    public void OnButtonClicked()
+    {
+        AudioSource.PlayClipAtPoint(_buttonClip, transform.position);
     }
 }
